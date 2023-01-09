936ee7a748ce706c2a31fac58b8d1568307a16c0
dom <714479614@qq.com>
2023-01-09 23:17:41 +0800

    chore: 增加区分环境相关配置

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,28 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-import {resolve} from 'path';
-import { viteMockServe } from "vite-plugin-mock";
+import { resolve } from 'path';
+import { viteMockServe } from 'vite-plugin-mock';
 
-export default defineConfig({
-    resolve: {
-        alias: {
-            '@': resolve('src')
-        }
-    },
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://jsonplaceholder.typicode.com',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '')
-            }
-        }
-    },
-    plugins: [vue(), viteMockServe({})]
-})
+export default (configEnv) => {
+    const viteEnv = loadEnv(configEnv.mode, process.cwd());
+    const { VITE_PUBLIC_PATH } = viteEnv;
+    return {
+        base: VITE_PUBLIC_PATH,
+        resolve: {
+            alias: {
+                '@': resolve('src'),
+            },
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://jsonplaceholder.typicode.com',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                },
+            },
+        },
+        plugins: [vue(), viteMockServe({})],
+    };
+};
