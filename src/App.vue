2fc09c58ac266e81a76b265b35ab5dd79fd70f67
dom <714479614@qq.com>
2023-06-07 22:17:55 +0800

    chore: change the config

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
     <div class="outer">Hello World</div>
-    <button @click="handleClick">{{state.count}}</button>
+    <button @click="handleClick">{{ state.count }}</button>
 </template>
 
 <script setup lang="ts">
-    import { reactive, defineProps } from 'vue'
-    defineProps({
-        msg: String
-    })
-    interface State {
-        count: number;
-    }
-    let state = reactive<State>({ count: 0 });
-    const handleClick = () => {
-        console.log(state.count);
-        state.count++;
-    }
+import { reactive, defineProps } from 'vue';
+defineProps({
+    msg: String,
+});
+interface State {
+    count: number;
+}
+const state = reactive<State>({ count: 0 });
+const handleClick = () => {
+    console.log(state.count);
+    state.count++;
+};
 
-    // 调用本地 Mock 接口
-fetch("/apiMock/get")
+// 调用本地 Mock 接口
+fetch('/apiMock/get')
     .then((response) => response.json())
     .then((json) => {
         console.log(`fetch('/api/todos/1')`, json);
-
     });
 </script>
 
